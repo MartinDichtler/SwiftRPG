0396ba7a5bbf793c873d281930f0d82f037114ee
Martin Dichtler <martin.dichtler@icloud.com>
2018-11-10 18:29:46 +0100

    Initial Commit

---
+++ SwiftRPG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftRPG
+//
+//  Created by Martin Dichtler on 10/11/2018.
+//  Copyright © 2018 Martin Dichtler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
